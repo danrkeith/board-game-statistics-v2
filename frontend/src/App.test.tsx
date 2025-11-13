fc7089ec9ae6fb9e59a01c025113da7021b57889
Daniel Keith <danrkeith@gmail.com>
2025-11-13 19:56:14 +1100

    Added testing

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,16 @@
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+
+test('should render navbar with title', () => {
+    render(<App />);
+
+    expect(screen.getByText('Board Game Statistics')).toBeInTheDocument();
+});
+
+test('should render home page by default', () => {
+    render(<App />);
+
+    expect(screen.getByText('Welcome!')).toBeInTheDocument();
+})
