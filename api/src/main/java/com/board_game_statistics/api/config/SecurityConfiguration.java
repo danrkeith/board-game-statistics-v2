4266f82ce2b796a9606456987f763614db737496
danrkeith <77320308+danrkeith@users.noreply.github.com>
2025-12-19 16:22:24 +1100

    Removed http from code (#5)

--- api/src/main/java/com/board_game_statistics/api/config/SecurityConfiguration.java
+++ api/src/main/java/com/board_game_statistics/api/config/SecurityConfiguration.java
@@ -56,7 +56,7 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://" + frontendUrl));
+        configuration.setAllowedOrigins(List.of(frontendUrl));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
