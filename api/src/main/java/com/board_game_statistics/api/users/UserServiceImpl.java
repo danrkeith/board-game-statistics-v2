b64ff60ec2f2a8ce030367bc64600a8653b32736
Daniel Keith <danrkeith@gmail.com>
2026-03-13 22:33:47 +1100

    Implemented edit

--- api/src/main/java/com/board_game_statistics/api/users/UserServiceImpl.java
+++ api/src/main/java/com/board_game_statistics/api/users/UserServiceImpl.java
@@ -26,8 +26,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User editUser(long id, String firstName, String lastName) {
-        // TODO - edit user
-        return getUser(id);
+        User user = getUser(id);
+
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+
+        return userRepository.save(user);
     }
 
     @Override
