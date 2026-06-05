d08b800d5a0ba154fb370a76dda07507b88fa534
Daniel Keith <danrkeith@gmail.com>
2026-06-05 19:20:51 +1000

    Fixed bug related to controller mappings

--- api/src/main/java/com/board_game_statistics/api/users/user_authorities/UserAuthoritiesController.java
+++ api/src/main/java/com/board_game_statistics/api/users/user_authorities/UserAuthoritiesController.java
@@ -7,11 +7,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
 
-@RestController("/users/{userId}/authorities")
+@RequestMapping("/users/{userId}/authorities")
+@RestController
 public class UserAuthoritiesController {
     private final UserAuthoritiesService userAuthoritiesService;
 
