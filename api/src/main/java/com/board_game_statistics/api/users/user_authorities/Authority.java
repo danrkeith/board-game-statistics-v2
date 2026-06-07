6cdd6df26570e53e559eed44219167ca35791b6a
Daniel Keith <danrkeith@gmail.com>
2026-06-07 16:38:21 +1000

    Added lombok to Authority

--- api/src/main/java/com/board_game_statistics/api/users/user_authorities/Authority.java
+++ api/src/main/java/com/board_game_statistics/api/users/user_authorities/Authority.java
@@ -1,5 +1,6 @@
 package com.board_game_statistics.api.users.user_authorities;
 
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Authority implements GrantedAuthority {
@@ -8,6 +9,7 @@ public enum Authority implements GrantedAuthority {
     MANAGE_GROUPS,
     MANAGE_GROUP_MEMBERSHIPS(MANAGE_USERS, MANAGE_GROUPS);
 
+    @Getter
     private final Authority[] prerequisites;
 
     Authority(Authority... prerequisites) {
@@ -17,9 +19,5 @@ public enum Authority implements GrantedAuthority {
     @Override
     public String getAuthority() {
         return name();
-    }
-
-    public Authority[] getPrerequisites() {
-        return prerequisites;
     }
 }
